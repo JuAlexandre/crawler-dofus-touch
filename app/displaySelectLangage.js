e627e98ac1adf7c2fa96318989a98808f0fc64e2
Julien <julien.alexandre26@gmail.com>
2019-12-06 16:42:50 +0100

    Clean code in displaySelectLangage.js file

--- app/displaySelectLangage.js
+++ app/displaySelectLangage.js
@@ -1,17 +1,15 @@
 const prompts = require('prompts');
 
 const displaySelectLangage = async () => {
-    const { langage } = await prompts([
-        {
-            type: 'select',
-            name: 'langage',
-            message: 'Choose your langage:',
-            choices: [
-                { title: 'English', value: 'EN' },
-                { title: 'French', value: 'FR' },
-            ],
-        }
-    ]);
+    const { langage } = await prompts({
+        type: 'select',
+        name: 'langage',
+        message: 'Choose your langage:',
+        choices: [
+            { title: 'English', value: 'EN' },
+            { title: 'French', value: 'FR' },
+        ],
+    });
 
     return langage;
 };
