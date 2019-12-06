463181abac6a81cbec8b6650cd93fdec9dbc03d4
Julien <julien.alexandre26@gmail.com>
2019-12-06 11:41:27 +0100

    Creation of the welcome box when users launch the script

--- app/index.js
+++ app/index.js
@@ -1,5 +1,17 @@
-const scrapingMounts = require('./scraping/mounts');
+const boxen = require('boxen');
+const chalk = require('chalk');
+
+const package = require('../package.json');
 
 (async () => {
-    const dragoturkeys = await scrapingMounts('EN');
+    const welcomeMessage = chalk.white('Welcome on');
+    const appName = chalk.green.bold('Dofus Touch Crawler');
+    const appVersion = chalk.gray(`v${package.version}`);
+
+    const welcomeBox = boxen(
+        `${welcomeMessage} ${appName} - ${appVersion}`,
+        { padding: 1, borderStyle: 'round' },
+    )
+
+    console.log(welcomeBox);
 })();
