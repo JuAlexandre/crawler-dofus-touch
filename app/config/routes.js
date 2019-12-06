73badd755e6c942a612247b34ab23efbb32b72c6
Julien <julien.alexandre26@gmail.com>
2019-12-06 10:29:03 +0100

    Creating a config file for encyclopedia routes

---
+++ app/config/routes.js
@@ -0,0 +1,8 @@
+const ENCYCLOPEDIA = {
+    MOUNTS: {
+        FR: 'https://www.dofus-touch.com/fr/mmorpg/encyclopedie/montures?display=table&size=96',
+        EN: 'https://www.dofus-touch.com/en/mmorpg/encyclopedia/mounts?display=table&size=96',
+    },
+};
+
+module.exports = ENCYCLOPEDIA;
