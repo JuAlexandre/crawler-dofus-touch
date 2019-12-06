760cd2f9ef246043e26e61201f62a7d635683638
Julien <julien.alexandre26@gmail.com>
2019-12-06 16:43:20 +0100

    Clean code in displaySelectOption.js file

--- app/displaySelectOption.js
+++ app/displaySelectOption.js
@@ -4,7 +4,7 @@ const displaySelectOption = async () => {
     const { option } = await prompts({
         type: 'select',
         name: 'option',
-        message: 'What do you want to do ?',
+        message: 'What do you want to do?',
         choices: [
             { title: 'Download dragoturkeys pictures', value: 'downloadDragoturkeysPictures' },
             { title: 'Get all dragoturkeys data', value: 'getAllDragoturkeysData' },
