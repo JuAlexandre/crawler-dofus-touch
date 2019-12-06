5849893b5c281e9571fd5aaa98ec5468582856b8
Julien <julien.alexandre26@gmail.com>
2019-12-06 17:51:36 +0100

    Refactoring functions requesting information to the user

--- app/index.js
+++ app/index.js
@@ -1,24 +1,20 @@
 const displayWelcomeBox = require('./viewing/displayWelcomeBox');
-const displaySelectLangage = require('./viewing/displaySelectLangage');
-const displaySelectOption = require('./viewing/displaySelectOption');
+const displayQuestions = require('./viewing/displayQuestions');
 const displayLoading = require('./viewing/displayLoading');
 const scrapingMounts = require('./scraping/mounts');
 
 (async () => {
     displayWelcomeBox();
-    const langage = await displaySelectLangage();
-    const option = await displaySelectOption();
-
+    const userAnswers = await displayQuestions();
     const spinner = displayLoading();
 
-    switch(option) {
+    switch(userAnswers.option) {
         case 'downloadDragoturkeysPictures':
             // TODO: Start a function to download all pictures.
             break;
         case 'getAllDragoturkeysData':
-            const dragoturkeys = await scrapingMounts(langage);
+            const dragoturkeys = await scrapingMounts(userAnswers.langage);
             dragoturkeys ? spinner.succeed('Done!') : spinner.fail('An error has occurred...');
-            console.log(dragoturkeys);
             break;
         default:
             spinner.stop();
---
+++ app/viewing/displayQuestions.js
@@ -0,0 +1,33 @@
+const prompts = require('prompts');
+
+const displayQuestions = async () => {
+    const userAnswers = await prompts([
+        {
+            type: 'select',
+            name: 'langage',
+            message: 'In which language do you want the results?',
+            choices: [
+                { title: 'English', value: 'EN' },
+                { title: 'French', value: 'FR' },
+            ],
+        },
+        {
+            type: 'select',
+            name: 'option',
+            message: 'What do you want to do?',
+            choices: [
+                { title: 'Download dragoturkeys pictures', value: 'downloadDragoturkeysPictures' },
+                { title: 'Get all dragoturkeys data', value: 'getAllDragoturkeysData' },
+            ],
+        },
+        {
+            type: prev => prev === 'downloadDragoturkeysPictures' ? 'number' : null,
+            name: 'picturesSize',
+            message: 'Which size would you like? (in pixel)',
+        }
+    ]);
+
+    return userAnswers;
+};
+
+module.exports = displayQuestions;
