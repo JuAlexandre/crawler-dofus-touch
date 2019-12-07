d9fd09c656f791214e383013306a28ffd0c4a0ba
Julien <julien.alexandre26@gmail.com>
2019-12-07 11:16:12 +0100

    Adding BASE_URL constant

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,4 +1,5 @@
 const constants = {
+    BASE_URL: 'https://www.dofus-touch.com',
     MOUNTS_URL: {
         FR: 'https://www.dofus-touch.com/fr/mmorpg/encyclopedie/montures?display=table&size=96',
         EN: 'https://www.dofus-touch.com/en/mmorpg/encyclopedia/mounts?display=table&size=96',
